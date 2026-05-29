f348167dc0ace569da44a096ddd3473a602fa8bf
vincentmartin84 <vinmart@outlook.fr>
2026-05-29 15:15:44 +0200

    request role succes

--- my_youtube_api/src/users/repositories/users.repository.ts
+++ my_youtube_api/src/users/repositories/users.repository.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { FindRelationsNotFoundError, Repository } from 'typeorm';
-import { FindManyOptions } from 'typeorm';
+import {
+  Repository,
+  FindManyOptions,
+  FindOneOptions,
+} from 'typeorm';
 
 import { User } from '../entities/user.entity';
 import { CreateUserDto } from '../dto/create-user.dto';
@@ -16,7 +19,6 @@ export class UsersRepository {
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
     const user = this.repository.create(createUserDto);
-
     return await this.repository.save(user);
   }
 
@@ -24,19 +26,30 @@ export class UsersRepository {
     return await this.repository.find(options);
   }
 
-  async findByEmail(email: string): Promise<User | null> {
+  async findByEmail(
+    email: string,
+    options?: FindOneOptions<User>,
+  ): Promise<User | null> {
     return await this.repository.findOne({
       where: { email },
+      ...options,
     });
   }
 
-  async findById(id: number, options?: FindManyOptions<User>): Promise<User | null> {
+  async findById(
+    id: number,
+    options?: FindOneOptions<User>,
+  ): Promise<User | null> {
     return await this.repository.findOne({
       where: { id },
-    }, options);
+      ...options,
+    });
   }
 
-  async updateUser(id: number, updateUserDto: UpdateUserDto): Promise<User | null> {
+  async updateUser(
+  id: number,
+  data: Partial<User>,
+): Promise<User | null> {
   const user = await this.repository.findOne({
     where: { id },
     relations: { roles: true },
@@ -46,12 +59,36 @@ export class UsersRepository {
     return null;
   }
 
-  Object.assign(user, updateUserDto);
+  Object.assign(user, data);
 
   return await this.repository.save(user);
 }
-  
-async deleteUser(id: number): Promise<void> {
+
+
+
+
+
+/*
+  async updateUser(
+    id: number,
+    updateUserDto: UpdateUserDto,
+  ): Promise<User | null> {
+    const user = await this.repository.findOne({
+      where: { id },
+      relations: { roles: true },
+    });
+
+    if (!user) {
+      return null;
+    }
+
+    Object.assign(user, updateUserDto);
+
+    return await this.repository.save(user);
+  }
+*/
+
+  async deleteUser(id: number): Promise<void> {
     await this.repository.delete(id);
   }
 }
