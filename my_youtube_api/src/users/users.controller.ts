31ae8f0db7606340b2074f52179412be3f7afabd
vincentmartin84 <vinmart@outlook.fr>
2026-05-30 16:45:15 +0200

    auth guard

--- my_youtube_api/src/users/users.controller.ts
+++ my_youtube_api/src/users/users.controller.ts
@@ -1,33 +1,40 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Request, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+    @UseGuards(AuthGuard)
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
-  @Get()
+
+@Get()
+  @UseGuards(AuthGuard)
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+    @UseGuards(AuthGuard)
   findOne(@Param('id') id: string) {
     return this.usersService.findById(+id);
   }
 
   @Patch(':id')
+    @UseGuards(AuthGuard)
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
+    @UseGuards(AuthGuard)
   remove(@Param('id') id: string) {
     return this.usersService.delete(+id);
   }
